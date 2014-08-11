73564d8dbe488d13f5d7981605f9abbf62e9c584
Jim Mertens <jbm120@case.edu>
2014-08-11 12:24:39 -0400

    add user infos

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ seminars
 ========
 
 CERCA Seminar Scheduling System
+--------
+
+Requires [`phpCAS`](https://wiki.jasig.org/display/casc/phpcas)
+
+To install me:
+ - Download me into a web directory.
+ - Create a MySQL DB, and create the tables in schema.sql.
+ - Set config info in assets/includes/config.tpl.php
+ - Set a phpCAS user in the settings table ("admins" field)
+ - rename the above config.tpl.php to config.php
--- schema.sql
+++ schema.sql
@@ -12,15 +12,41 @@ CREATE TABLE `seminars` (
   `date` datetime NOT NULL,
   `announced` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `value` text NOT NULL,
   `description` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("admins", "", "Comma separated list of phpCAS IDs of people with administrative permissions");
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("email", "", "Automated Email reply-to Address");
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("titlemail", 335, "Number of hours before talk to ask for titles");
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("titleminder", 96, "Number of hours before talk to ask again about titles");
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("announcetime", 15, "Number of hours before seminar to send email announcement");
+INSERT INTO `settings`
+  (`name`, `value`, `description`)
+  VALUES
+  ("lastupdate", 0, "Unix Timestamp of last job run time");
 
 CREATE TABLE `talks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -30,4 +56,4 @@ CREATE TABLE `talks` (
   `edit_key` varchar(40) NOT NULL,
   `keymailed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
