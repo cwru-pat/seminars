b5399bb7003f3d77b7acb77e9f0bebf5937d7dba
Jim Mertens <jbm120@case.edu>
2014-12-09 10:31:16 -0500

    take that, tabs

--- assets/jobs/mailer.job.php
+++ assets/jobs/mailer.job.php
@@ -7,7 +7,7 @@ $now = time();
 $settings = $mysqli->dbQuery("SELECT * FROM settings");
 $conf = Array();
 foreach($settings as $setting) {
-	$conf[$setting->name] = trim($setting->value);
+    $conf[$setting->name] = trim($setting->value);
 }
 
 // all notifications that go out will be for talks that happen after now.
@@ -15,13 +15,13 @@ $seminars = $mysqli->dbQuery("SELECT * FROM seminars WHERE date > FROM_UNIXTIME(
 if(count($seminars) == 0) { die("No upcoming talks."); }
 
 foreach($seminars as $seminar) {
-	$seminar = get_object_vars($seminar);
+    $seminar = get_object_vars($seminar);
 
-	// times after which notifications should be mailed
-	$seminar_time = strtotime($seminar['date']);
-	$title_mail_time = $seminar_time - $conf['titlemail']*3600;
-	$title_reminder_time = $seminar_time - $conf['titleminder']*3600;
-	$announce_time = $seminar_time - $conf['announcetime']*3600;
+    // times after which notifications should be mailed
+    $seminar_time = strtotime($seminar['date']);
+    $title_mail_time = $seminar_time - $conf['titlemail']*3600;
+    $title_reminder_time = $seminar_time - $conf['titleminder']*3600;
+    $announce_time = $seminar_time - $conf['announcetime']*3600;
 
 
 /** 
@@ -31,15 +31,15 @@ HANDLE TITLE REQUESTS
 **/
 
 
-	$talks = $mysqli->dbQuery("SELECT title, keymailed, name, email, edit_key, t.id FROM (SELECT * FROM talks WHERE seminar='" . $seminar['id'] . "') as t LEFT JOIN presenters as p ON t.presenter=p.id");
-	foreach($talks as $talk) {
-		$talk = get_object_vars($talk);
-		$email = "";
+    $talks = $mysqli->dbQuery("SELECT title, keymailed, name, email, edit_key, t.id FROM (SELECT * FROM talks WHERE seminar='" . $seminar['id'] . "') as t LEFT JOIN presenters as p ON t.presenter=p.id");
+    foreach($talks as $talk) {
+        $talk = get_object_vars($talk);
+        $email = "";
 
-		// if (notifytime < now  && notified == 0)
-		// send notify email
-		if($now > $title_mail_time && $talk['keymailed'] == 0) {
-			$email = "
+        // if (notifytime < now  && notified == 0)
+        // send notify email
+        if($now > $title_mail_time && $talk['keymailed'] == 0) {
+            $email = "
 Hey there!
 
 You're scheduled to give a talk at the CERCA seminar on ".date("M jS, Y", $seminar_time).".  Before then, you'll need to submit a title that will go out in the CERCA reminder email.
@@ -50,12 +50,12 @@ You can change this title as often as you like up until the time of your talk, b
 
 --This is an automated message; please reply to " . $conf['email'] . " if you need help or if you need to reschedule your talk.
 ";
-		}
+        }
 
-		// if (secondtime < now  && notified <= 1 && no title yet)
-		// send followup email
-		if($now > $title_reminder_time && $talk['keymailed'] == 1 && trim($talk['title']) == "") {
-			$email = "
+        // if (secondtime < now  && notified <= 1 && no title yet)
+        // send followup email
+        if($now > $title_reminder_time && $talk['keymailed'] == 1 && trim($talk['title']) == "") {
+            $email = "
 Hey there!
 
 This is a reminder email - you're scheduled to give a talk at the CERCA seminar on ".date("M jS, Y", $seminar_time).".
@@ -66,28 +66,28 @@ The CERCA announcement will be sent out at ".date("g:i a \\o\\n M jS", $announce
 
 --This is an automated message; please reply to ". $conf['email'] . " if you need help or if you need to reschedule your talk.
 ";
-		}
+        }
 
-		if($email) {
-			// send email
-			
-			$subject = "CERCA Seminar: Submit your talk title!";
-			$org_email = $conf['email'];
-			$to = $talk['email'];
-			$headers = 'From: "CERCA Notices" <no-reply@anduril.phys.cwru.edu>' . "\r\n" .
-					    'Reply-To: ' . $org_email . "\r\n" .
-					    'Cc: ' . $org_email . "\r\n";
+        if($email) {
+            // send email
+            
+            $subject = "CERCA Seminar: Submit your talk title!";
+            $org_email = $conf['email'];
+            $to = $talk['email'];
+            $headers = 'From: "CERCA Notices" <no-reply@anduril.phys.cwru.edu>' . "\r\n" .
+                        'Reply-To: ' . $org_email . "\r\n" .
+                        'Cc: ' . $org_email . "\r\n";
 
-			$success = mail($to, $subject, $email, $headers);
-			//$success = TRUE;
-			if($success) {
-				print "Mail sent!";
-				$mysqli->dbCommand("UPDATE talks SET keymailed = keymailed+1 WHERE id='".$talk[id]."'");
-			} else {
-				print "Mail failed!";
-			}
-		}
-	}
+            $success = mail($to, $subject, $email, $headers);
+            //$success = TRUE;
+            if($success) {
+                print "Mail sent!";
+                $mysqli->dbCommand("UPDATE talks SET keymailed = keymailed+1 WHERE id='".$talk[id]."'");
+            } else {
+                print "Mail failed!";
+            }
+        }
+    }
 /** 
 
 HANDLE SEMINAR ANNOUNCEMENTS
@@ -121,8 +121,8 @@ Just a reminder that the next CERCA seminar is in about an hour. We'll be hearin
         $org_email = $conf['email'];
         $to = "CERCA List <cerca@case.edu>";
         $headers = 'From: "CERCA Notices" <no-reply@anduril.phys.cwru.edu>' . "\r\n" .
-				    'Reply-To: ' . $org_email . "\r\n" .
-				    'Cc: ' . $org_email . "\r\n";
+                    'Reply-To: ' . $org_email . "\r\n" .
+                    'Cc: ' . $org_email . "\r\n";
         if($email) {
             $success = mail($to, $subject, $email, $headers);
             if($success) {
@@ -137,47 +137,47 @@ Just a reminder that the next CERCA seminar is in about an hour. We'll be hearin
     }
     
     
-	// if (cercannounce < now && announced == 0)
-		// announce seminar talks
+    // if (cercannounce < now && announced == 0)
+        // announce seminar talks
     if($announce_time < $now && $seminar['announced'] == 0) {
-		$email = "
+        $email = "
 Hello,
 
 For the next CERCA seminar (".date("M jS \\a\\t G:i", $seminar_time)."), we'll be hearing from the following:
 ";
 
-		$talks = $mysqli->dbQuery("SELECT * FROM (SELECT * FROM talks WHERE seminar='".$seminar['id']."') AS t LEFT JOIN presenters ON t.presenter=presenters.id");
-		foreach($talks as $talk) {
-			$email .= "* " . ($talk->name) . ($talk->title ? (" on '" . $talk->title . "'") : "") . "\n";
-		}
-		$email .= "\nYou can see the schedule of talks at http://cerca.case.edu/pizza.php.";
-		$email .= "\nHope to see you there!\n";
+        $talks = $mysqli->dbQuery("SELECT * FROM (SELECT * FROM talks WHERE seminar='".$seminar['id']."') AS t LEFT JOIN presenters ON t.presenter=presenters.id");
+        foreach($talks as $talk) {
+            $email .= "* " . ($talk->name) . ($talk->title ? (" on '" . $talk->title . "'") : "") . "\n";
+        }
+        $email .= "\nYou can see the schedule of talks at http://cerca.case.edu/pizza.php.";
+        $email .= "\nHope to see you there!\n";
 
 
-		// make sure there are actually talks this week.
-		foreach($talks as $talk) {
-			if($talk->name == "NO CERCA") {
-				$email = "Hello - Just a reminder that there will be NO CERCA this week, but stay tuned for future talks!";
-			}
-		}
+        // make sure there are actually talks this week.
+        foreach($talks as $talk) {
+            if($talk->name == "NO CERCA") {
+                $email = "Hello - Just a reminder that there will be NO CERCA this week, but stay tuned for future talks!";
+            }
+        }
 
 
-		$subject = "CERCA Seminar";
-		$org_email = $conf['email'];
-		$to = "CERCA List <cerca@case.edu>";
-		$headers = 'From: "CERCA Notices" <no-reply@anduril.phys.cwru.edu>' . "\r\n" .
-				    'Reply-To: ' . $org_email . "\r\n" .
-				    'Cc: ' . $org_email . "\r\n";
+        $subject = "CERCA Seminar";
+        $org_email = $conf['email'];
+        $to = "CERCA List <cerca@case.edu>";
+        $headers = 'From: "CERCA Notices" <no-reply@anduril.phys.cwru.edu>' . "\r\n" .
+                    'Reply-To: ' . $org_email . "\r\n" .
+                    'Cc: ' . $org_email . "\r\n";
 
-		$success = mail($to, $subject, $email, $headers);
-		if($success) {
-			print "Mail sent!";
-			$mysqli->dbCommand("UPDATE seminars SET announced=1 WHERE id='".$seminar['id']."'");
-		} else {
-			print "Mail failed!";
-		}
-	}
-	
+        $success = mail($to, $subject, $email, $headers);
+        if($success) {
+            print "Mail sent!";
+            $mysqli->dbCommand("UPDATE seminars SET announced=1 WHERE id='".$seminar['id']."'");
+        } else {
+            print "Mail failed!";
+        }
+    }
+    
 
 }
 
