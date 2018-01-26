d433fe7bf7e955c68a16b203469f1c9c2406a271
deskinsjt <jtd55@case.edu>
2018-01-26 16:29:42 -0500

    Added logging and better cron mail handeling stuffs.

---
+++ assets/jobs/seminar_cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# To automate this, set up a cron job, similar to
+# PATH=/usr/sbin:/usr/bin:$PATH
+# */15 * * * * /PATH_TO_WEB_DIRECTORY/includes/jobs/seminars_cron.sh >> /PATH_TO_WEB_DIRECTORY/includes/log/cron.log
+
+# which will run every 15 minutes.
+
+my_dir=$(cd `dirname $0` && pwd)
+cd $my_dir
+touch log/mail.log log/update.log log/cron.log log/logrotate.log
+php mailer.job.php >> log/mail.log
+php runupdate.job.php >> log/update.log
+logrotate -s log/logrotate.log logrotate.conf
