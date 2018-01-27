9a7c255335d78d3cf9e6f460dd3e587f42500591
deskinsjt <jtd55@case.edu>
2018-01-26 21:30:25 -0500

    fixed a lastupdate disabled?

--- settings.php
+++ settings.php
@@ -28,7 +28,7 @@ if(isset($_REQUEST['submit']) && isset($_REQUEST['CSRFToken'])
 		print "<tr><td>";
 		print o($form_values['description']);
 		print "</td><td>";
-		print "<input type='text' value='" . o($form_values['value']) . "' name='" . o($form_values['name']) . ($form_values['name'] == "lastupdate" ? "' disabled'":"") . "' />";
+		print "<input type='text' value='" . o($form_values['value']) . "' name='" . o($form_values['name']) . ($form_values['name'] == "lastupdate" ? "disabled":"") . "/>";
 		print "</td></tr>";
 	}
 	?>
